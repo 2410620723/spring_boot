e878cf04b37c25ae5a19083760de578a6e30ab55
zxm <zxm@zxm-PC>
2017-01-19 14:01:15 +0800

    springboot

--- springboot/src/main/java/com/spring/boot/controller/TemplatesController.java
+++ springboot/src/main/java/com/spring/boot/controller/TemplatesController.java
@@ -2,15 +2,27 @@ package com.spring.boot.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.boot.service.PersonService;
 
 @Controller
 @RequestMapping("/temp")
 public class TemplatesController {
+	@Autowired
+	private PersonService personService;
+	
 	@RequestMapping("/hello")
 	public String hello(Map<String,Object> result){
 		result.put("name", "周兴美");
 		return "hello";
+	}
+	
+	@RequestMapping("/getAll")
+	public String getAll(Map<String,Object> map){
+		map.put("persons", personService.getAll());
+		return "personList";
 	}
 }
